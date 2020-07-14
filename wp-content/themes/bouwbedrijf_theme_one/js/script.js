a30bf2610409de87b390f23043c1d2720a472110
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-07-14 17:05:11 +0200

    hamburger animation added

--- wp-content/themes/bouwbedrijf_theme_one/js/script.js
+++ wp-content/themes/bouwbedrijf_theme_one/js/script.js
@@ -3,6 +3,7 @@ const menu = document.querySelector(".header__menu");
 
 const toggleMenu = () => {
   menu.classList.toggle("open");
+  hamburgerIcon.classList.toggle("open");
 };
 
 hamburgerIcon.addEventListener("click", toggleMenu);
