fb9dfae784187ec6ed37fb8e88bf3ce5e3d4d146
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-07-15 12:17:48 +0200

    added animatio for mobile nav items

--- wp-content/themes/bouwbedrijf_theme_one/js/script.js
+++ wp-content/themes/bouwbedrijf_theme_one/js/script.js
@@ -1,9 +1,11 @@
 const hamburgerIcon = document.querySelector(".header__hamburger");
 const menu = document.querySelector(".mobilemenu");
+const navitems = document.querySelector(".mobilemenu__list");
 
 const toggleMenu = () => {
   menu.classList.toggle("open");
   hamburgerIcon.classList.toggle("open");
+  navitems.classList.toggle("open");
 };
 
 hamburgerIcon.addEventListener("click", toggleMenu);
