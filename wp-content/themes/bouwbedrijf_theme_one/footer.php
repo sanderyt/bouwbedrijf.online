fb71fd14b9c6d13a5ded8803a72234b920ac94aa
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-07-16 22:01:34 +0200

    fixed footer bug

--- wp-content/themes/bouwbedrijf_theme_one/footer.php
+++ wp-content/themes/bouwbedrijf_theme_one/footer.php
@@ -1,28 +1,32 @@
 <footer>
-<div class="container">
-<div class="footer row">
-      <div class="footer__menu col-md-4">
-      <h2>Contact opnemen</h2>
-      <ul>
-      <li><i class="fas fa-phone"></i><?php echo get_theme_mod('business_phone')?></li><br>
-      <li><i class="fas fa-envelope"></i><?php echo get_theme_mod('business_email')?></li><br>
-      <li><i class="fas fa-home"></i><?php echo get_theme_mod('business_street')?>, <?php echo get_theme_mod('business_postalcode')?>, <?php echo get_theme_mod('business_residence')?></li><br>
-      </ul>
+      <div class="footer">
+            <div class="container">
+                  <div class="row">
+                        <div class="footer__menu col-md-4">
+                              <h2>Contact opnemen</h2>
+                              <ul>
+                              <li><i class="fas fa-phone"></i><?php echo get_theme_mod('business_phone')?></li><br>
+                              <li><i class="fas fa-envelope"></i><?php echo get_theme_mod('business_email')?></li><br>
+                              <li><i class="fas fa-home"></i><?php echo get_theme_mod('business_street')?>, <?php echo get_theme_mod('business_postalcode')?>, <?php echo get_theme_mod('business_residence')?></li><br>
+                              </ul>
+                        </div>
+                        <div class="footer__menu col-md-4">
+                              <h2>Werkzaamheden</h2>
+                              <?php wp_nav_menu( array( 'theme_location' => 'footer-menu1', 'container_class' => 'footermenu' ) ); ?>
+                        </div>
+                        <div class="footer__menu col-md-4">
+                              <h2>Projecten</h2>
+                              <?php wp_nav_menu( array( 'theme_location' => 'footer-menu2', 'container_class' => 'footermenu' ) ); ?>
+                        </div>
+                  </div>
       </div>
-      <div class="footer__menu col-md-4">
-      <h2>Werkzaamheden</h2>
-      <?php wp_nav_menu( array( 'theme_location' => 'footer-menu1', 'container_class' => 'footermenu' ) ); ?>
-      </div>
-      <div class="footer__menu col-md-4">
-      <h2>Projecten</h2>
-      <?php wp_nav_menu( array( 'theme_location' => 'footer-menu2', 'container_class' => 'footermenu' ) ); ?>
-      </div>
-      </div>
-      <div class="footer__bottom row">
+    </div>
+    </div>
+    <div class="container-fluid">
+    <div class="footer__bottom row">
             <div class="col d-flex justify-content-center">
                   <span>Deze website is gerealiseerd door <a href="http://www.bouwbedrijf.online" class="link--primary" target="_blank">bouwbedrijf.online</a></span>
             </div>
-      </div>
     </div>
     </footer>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
